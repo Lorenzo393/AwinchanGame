1853d0de5fa49b279340faf6b305abc9c37107d9
Lorenzo393 <loloc.gelos@outlook.com>
2025-04-29 11:21:49 -0300

    Agrego sistema de stamina

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,7 +1,9 @@
 using System.Numerics;
 using Unity.Collections;
 using Unity.VisualScripting;
+using UnityEngine.UI;
 using UnityEngine;
+
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,7 +14,7 @@ public class PlayerController : MonoBehaviour
     public float speedRun = 10.0f; // REFERENCE VAL
     public float jumpH = 3.0f; // REFERENCE VAL
     public CharacterController player;
-
+    
     public Transform groundCheck;
     public LayerMask groundMask;
     public float groundDist = 0.3f; // REFERENCE VAL
@@ -21,24 +23,69 @@ public class PlayerController : MonoBehaviour
     public float gravity = -9.8f; // REFERENCE VAL
     private UnityEngine.Vector3 velocity;
     
-    void Start(){
+    
+    public float maxStamina = 120f;
+    private float currentStamina;
+    public float staminaDrainRate = 25f; // Xs
+    public float staminaRegenRate = 15f; // Xs
+    public Slider staminaSlider;
+    public Image staminaFillImage;
+
+    void Start()
+    {
         player = GetComponent<CharacterController>();
+        currentStamina = maxStamina;
+
+        if (staminaSlider != null)
+            staminaSlider.maxValue = maxStamina;
     }
-    void Update(){
-        isGrounded = Physics.CheckSphere(groundCheck.position,groundDist,groundMask);
-        if(isGrounded && velocity.y < 0) velocity.y = -2f;
-        if(Input.GetButtonDown("Jump") && isGrounded) velocity.y = Mathf.Sqrt(jumpH * -2f * gravity);
+
+    void Update()
+    {
+        // Ground check
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDist, groundMask);
+        if (isGrounded && velocity.y < 0) velocity.y = -2f;
+
+        // Jumping
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            velocity.y = Mathf.Sqrt(jumpH * -2f * gravity);
         velocity.y += gravity * Time.deltaTime;
 
+        // Input
         hor = Input.GetAxis("Horizontal");
         ver = Input.GetAxis("Vertical");
-
         playerInput = transform.right * hor + transform.forward * ver;
-        playerInput = UnityEngine.Vector3.ClampMagnitude(playerInput,1);
+        playerInput = UnityEngine.Vector3.ClampMagnitude(playerInput, 1);
 
-        float speed = Input.GetKey(KeyCode.LeftShift) ? speedRun : speedNormal;
+        // Sprint + Stamina
+        bool isSprinting = Input.GetKey(KeyCode.LeftShift) && currentStamina > 0 && playerInput.magnitude > 0;
+        float speed = isSprinting ? speedRun : speedNormal;
 
+        if (isSprinting)
+        {
+            currentStamina -= staminaDrainRate * Time.deltaTime;
+            if (currentStamina < 0) currentStamina = 0;
+        }
+        else if (isGrounded) // regenerate grouded
+        {
+            currentStamina += staminaRegenRate * Time.deltaTime;
+            if (currentStamina > maxStamina) currentStamina = maxStamina;
+        }
+
+        // Update UI
+        if (staminaSlider != null) staminaSlider.value = currentStamina;
+
+        if (staminaFillImage != null){
+        float staminaPercent = currentStamina / maxStamina;
+
+        if (staminaPercent > 0.5f) staminaFillImage.color = Color.green;
+        else if (staminaPercent > 0.2f) staminaFillImage.color = Color.yellow;
+        else staminaFillImage.color = Color.red;
+        }
+
+
+        // Movement
         player.Move(speed * Time.deltaTime * playerInput);
-        player.Move(velocity*Time.deltaTime);
+        player.Move(velocity * Time.deltaTime);
     }
 }
