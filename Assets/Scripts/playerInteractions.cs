c3f857192799a686be1855e0a9f991b77bc07215
Lorenzo393 <loloc.gelos@outlook.com>
2025-04-29 19:00:25 -0300

    Control basico de linterna

---
+++ Assets/Scripts/playerInteractions.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class playerInteractions : MonoBehaviour
+{
+    public GameObject bloque;
+    public GameObject telefono;
+    public Transform playerCameraTransform;
+    public LayerMask pickUpLayerMask;
+    private bool linternaEncendida;
+    private bool linternaObtenida;
+
+    void Start(){
+        linternaEncendida = false;
+        linternaObtenida = true;
+    }
+
+    void Update(){
+        if(Input.GetMouseButtonDown(0)){ //Click izq
+            if(Physics.Raycast(playerCameraTransform.position,playerCameraTransform.forward,out RaycastHit raycastHit,2f)){
+                //CONTINUAR
+            }
+        }
+        if(Input.GetMouseButtonDown(1) && linternaObtenida){ //Click der
+            linternaEncendida = !linternaEncendida;
+        }
+        telefono.SetActive(linternaEncendida);
+    }
+}
