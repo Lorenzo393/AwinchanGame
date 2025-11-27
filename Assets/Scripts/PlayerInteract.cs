3e49040fc3f85d4341c94bcdcb1ac30804ff4334
Lorenzo393 <loloc.gelos@outlook.com>
2025-11-27 11:05:09 -0300

    Agrego interacciones generales con objetos

---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    // Referencia al objeto que tiene el script gameInput
+    [SerializeField] private GameInput gameInput;
+    // Referencia de la camara para ver hacia donde esta viendo el jugador
+    [SerializeField] private new Transform camera;
+    // Capa en las que puede interactuar el jugador
+    [SerializeField] private LayerMask interactableLayerMask;
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e){
+        PlayerInteraction();
+    }
+
+    private void Start(){
+        // Evento interaccion
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+    private void PlayerInteraction(){
+        float interactDistance = 3.0f;
+        if(Physics.Raycast(camera.position,camera.forward,out RaycastHit raycastHit, interactDistance, interactableLayerMask)){
+            Debug.Log(raycastHit.transform);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using Unity.IntegerTime;
+using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+    // Character controller
+    private CharacterController characterController;
+    // Referencia al objeto que tiene el script gameInput
+    [SerializeField] private GameInput gameInput;
+    // Velocidad a la que camina el personaje
+    [SerializeField] private float walkingSpeed = 4.0f;
+    // Velocidad a la que corre el personaje
+    [SerializeField] private float runningSpeed = 8.0f;
+    // Referencia de la camara para ver hacia donde esta viendo el jugador
+    [SerializeField] private new Transform camera;
+    // Referencia al telefono
+    [SerializeField] private Transform phone;
+
+
+    // Velocidad actual del jugador
+    private float currentSpeed;
+    // Fuerza de la gravedad/ caida del personaje
+    private const float gravityForce = -20.0f;
+    // Fuerza de empuje normal/ fuerza que se aplica con el personaje tocando el suelo 
+    private const float groundStickForce = -2.0f;
+    // Luz del telefono prendido/apagado
+    private bool phoneLight = false;
+    private void GameInput_OnSprintActionStarted(object sender, System.EventArgs e){
+        currentSpeed = runningSpeed;
+    }
+
+    private void GameInput_OnSprintActionCanceled(object sender, System.EventArgs e){
+        currentSpeed = walkingSpeed;
+    }
+    private void GameInput_OnFlashlightAction(object sender, System.EventArgs e)
+    {
+        if(phoneLight == false){
+            phoneLight = true;
+        }
+        else if(phoneLight == true){
+            phoneLight = false;
+        }
+    }
+    private void Start(){
+        // Inicializa el character controller
+        characterController = GetComponent<CharacterController>();
+
+        // Inicializo la velocidad de movimiento actual
+        currentSpeed = walkingSpeed;
+
+        // Evento correr iniciado
+        gameInput.OnSprintActionStarted += GameInput_OnSprintActionStarted;
+        // Evento correr terminado
+        gameInput.OnSprintActionCanceled += GameInput_OnSprintActionCanceled;
+        // Evento linterna
+        gameInput.OnFlashlightAction += GameInput_OnFlashlightAction;
+    }
+    private void Update(){
+        // Mueve al personaje
+        HandleMovement();
+        phone.gameObject.SetActive(phoneLight);
+    }
+    private void HandleMovement(){
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 playerMovement = (camera.forward * inputVector.y) + (camera.right * inputVector.x);
+        playerMovement.y = 0f;
+
+        // Multiplico la direccion de movimiento con la velocidad actual
+        playerMovement *= GetCurrentSpeed();
+
+        // Aplico gravedad sin que sea multiplicada por la velocidad del jugador
+        playerMovement.y = characterController.isGrounded? groundStickForce : gravityForce;
+
+        // Mueve al personaje multiplicando su movimiento por deltaTime
+        characterController.Move(playerMovement * Time.deltaTime);
+    }
+    private float GetCurrentSpeed(){
+        return currentSpeed;
+    }
+
+}
