e46b090158014cfd73fdab63ed5a9948b4bc3d88
Lorenzo393 <loloc.gelos@outlook.com>
2026-01-07 10:18:47 -0300

    Agrego animacion de iniciar la partida

--- Assets/Scripts/Main Menu/EnterGameAnimation.cs
+++ Assets/Scripts/Main Menu/EnterGameAnimation.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.ProBuilder.Shapes;
 using UnityEngine.SceneManagement;
 
 public class EnterGameAnimation : MonoBehaviour
@@ -7,7 +9,7 @@ public class EnterGameAnimation : MonoBehaviour
     [SerializeField] private GameObject camera1;
     [SerializeField] private GameObject camera2;
     [SerializeField] private GameObject camera3;
-    [SerializeField] private Transform door;
+    [SerializeField] private GameObject door;
 
     private void MainMenuButtons_OnButtonPlayClicked(object sender, System.EventArgs e){
         StartCoroutine(CameraAnimation());
@@ -18,8 +20,30 @@ public class EnterGameAnimation : MonoBehaviour
     }
 
     IEnumerator CameraAnimation(){
-        Debug.Log("Animation");
+        // Ocultar el cursor
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        // Pasa a camara 2
+        camera1.SetActive(false);
+        yield return new WaitForSecondsRealtime(3.0f);
+
+        // Animacion puerta
+        DoorHandler doorHandler = door.GetComponent<DoorHandler>();
+        doorHandler.Interact();
+        yield return new WaitForSecondsRealtime(1.0f);
+
+        // Pasa a camara 3
+        camera2.SetActive(false);
+        yield return new WaitForSecondsRealtime(2.0f);
+        yield return StartCoroutine(FadeAnimation.Instance.FadeIn());
+        yield return new WaitForSecondsRealtime(1.0f);
+
+        // Carga escena nueva
         SceneManager.LoadScene(1);
+
         yield return null;
     }
+
+    
 }
--- Assets/Scripts/Scripter/LadderAnimation.cs
+++ Assets/Scripts/Scripter/LadderAnimation.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using UnityEditor.PackageManager.UI;
 using UnityEngine;
 
 public class LadderAnimation : MonoBehaviour
