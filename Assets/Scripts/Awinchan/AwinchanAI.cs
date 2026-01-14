1ef3532e69ae997d8a9e24358e4f2be777343420
Lorenzo393 <loloc.gelos@outlook.com>
2026-01-14 12:58:55 -0300

    Agrego roaming del enemigo

--- Assets/Scripts/Awinchan/AwinchanAI.cs
+++ Assets/Scripts/Awinchan/AwinchanAI.cs
@@ -1,25 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class AwinchanAI : MonoBehaviour
 {
-    private enum State{
+    private enum AwinchanStates{
+        Disability,
         Roaming,
         Cheasing,
         Attack,
     }
-    private State state;
+    private AwinchanStates awinchanState;
+    [SerializeField] private Transform direction;
+    [Header ("SpotsPositions")]
+    [SerializeField] private Transform hiddenPosition;
+    [SerializeField] private Transform firstPosition;
+    [SerializeField] private Transform spawnPosition;
+    [Header ("RoamingPositions")]
+    [SerializeField] List<Transform> roamingPositionsList;
+    [SerializeField]private Vector3 roamDirection;
+    [SerializeField] private float reachedPositionDistance = 2f;
+
+    private NavMeshAgent navMeshAgent;
     private void Awake(){
-        state = State.Roaming;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        
+        navMeshAgent.enabled = false;
+        TeleportAwinchan(hiddenPosition);
+        awinchanState = AwinchanStates.Disability;
+
+        roamDirection = GetRoamingPosition();
+        StartCoroutine(Test());
     }
     private void Update(){
-        switch (state){
-            case State.Roaming:
+        switch (awinchanState){
+            case AwinchanStates.Roaming:
+                navMeshAgent.destination = roamDirection;
+                //direction.position = roamDirection;
+                if(Vector3.Distance(transform.position, roamDirection) < reachedPositionDistance){
+                    roamDirection = GetRoamingPosition();
+                }
                 break;
-            case State.Cheasing:
+            case AwinchanStates.Cheasing:
                 break;
-            case State.Attack:
+            case AwinchanStates.Attack:
+
                 break;
+            case AwinchanStates.Disability:
+                break;
+            
         }
+    }
+
+    IEnumerator Test(){
+
+        yield return new WaitForSecondsRealtime(2.0f);
+        TeleportAwinchan(firstPosition);
+        navMeshAgent.enabled = true;
+
+        yield return new WaitForSecondsRealtime(5.0f);
+        navMeshAgent.enabled = false;
+        TeleportAwinchan(spawnPosition);
+        navMeshAgent.enabled = true;
+        awinchanState = AwinchanStates.Roaming;
+
+        yield return null;
+    }
+    
+    private void TeleportAwinchan(Transform spot){
+        this.transform.SetPositionAndRotation(spot.position, spot.rotation);
+    }
+
+    private Vector3 GetRoamingPosition(){
+        return roamingPositionsList[Random.Range(0,roamingPositionsList.Count)].position;
     }
 
 }
