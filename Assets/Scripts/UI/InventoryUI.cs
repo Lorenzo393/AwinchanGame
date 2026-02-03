d7f4b334a711cdece8c734d2806112cf80421a4f
Lorenzo393 <loloc.gelos@outlook.com>
2026-02-03 11:40:52 -0300

    Agregados de fonts y visuales

--- Assets/Scripts/UI/InventoryUI.cs
+++ Assets/Scripts/UI/InventoryUI.cs
@@ -3,16 +3,35 @@ using TMPro;
 
 public class InventoryUI : MonoBehaviour
 {
+    /*
+        Null,
+        UnoCuatro,
+        DosCinco,
+        DosSeis,
+        Principals,
+        Library,
+        Outside,
+    */
     private TextMeshProUGUI inventoryText;
     private void PlayerInventory_OnCurrentKeyModification(object sender, System.EventArgs e){
-        inventoryText.text = "Inventario: " + PlayerInventory.Instance.GetCurrentKey();
+        string actualKey = PlayerInventory.Instance.GetCurrentKey().ToString();
+        inventoryText.text = actualKey switch
+        {
+            "UnoCuatro" => "Inventory: " + "1-4",
+            "DosCinco" => "Inventory: " + "2-5",
+            "DosSeis" => "Inventory: " + "2-6",
+            "Principals" => "Inventory: " + "Principals",
+            "Library" => "Inventory: " + "Library",
+            "Outside" => "Inventory: " + "Outside",
+            _ => "Inventory: " + actualKey,
+        };
     }
     private void Awake(){
         inventoryText = GetComponent<TextMeshProUGUI>();
     }
     private void Start(){
         // LLave inicial
-        inventoryText.text = "Inventario: " + PlayerInventory.Instance.GetCurrentKey();
+        inventoryText.text = "Inventory: " + PlayerInventory.Instance.GetCurrentKey();
         // Evento modificacion del la llave actual
         PlayerInventory.Instance.OnCurrentKeyModification += PlayerInventory_OnCurrentKeyModification;
     }
